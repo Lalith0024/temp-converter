949517402632dba47cc6450d52fa7502877bddf4
Lalith0024 <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-17 20:43:08 +0530

    changed some if-else conditions for more realistic

--- index.js
+++ index.js
@@ -5,17 +5,41 @@ const result = document.getElementById("result");
 
 function convert() {
     let temp;
+    
+    if (textbox.value === "") {
+        result.textContent = "Please enter some value 🤓";
+        result.classList.remove('show');
+        setTimeout(() => result.classList.add('show'), 250);
+        return; // Exit the function early if no value is entered
+    }
+    
     if (radio1.checked) {
         temp = Number(textbox.value);
         temp = temp * 9/5 + 32;
-        result.textContent = temp.toFixed(1) + " °F";
+        if (temp > 68){
+            result.textContent = temp.toFixed(1) + " °F" + "🥵";
+            
+        }
+        else{
+            result.textContent = temp.toFixed(1) + " °F" + "🥶";
+        }
     } else if (radio2.checked) {
         temp = Number(textbox.value);
         temp = (temp - 32) * 5/9;
-        result.textContent = temp.toFixed(1) + " °C";
+        if (temp > 68){
+            result.textContent = temp.toFixed(1) + " °C" + "🥵";
+        }
+        else{
+            result.textContent = temp.toFixed(1) + " °C" + "🥶";
+        }
     } else {
-        result.textContent = "PLEASE SELECT A UNIT";
+        result.textContent = "PLEASE SELECT A UNIT 🥹";
     }
     result.classList.remove('show');
     setTimeout(() => result.classList.add('show'), 250);
+    if (isNaN(textbox.value)){
+        result.textContent = "PLEASE ENTER A VALID INTEGER ☝️"
+        
+    }
 }
+
