ddd1078f5153113199b68854d78ddb573fbecdc1
Lalith0024 <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-17 18:28:51 +0530

    final commit

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-const textbox = document.getElementById("textbox")
-const radio1 = document.getElementById("tofar")
-const radio2 = document.getElementById("tocel")
-const result = document.getElementById("result")
+const textbox = document.getElementById("textbox");
+const radio1 = document.getElementById("tofar");
+const radio2 = document.getElementById("tocel");
+const result = document.getElementById("result");
 
-let temp;
-
-function convert(){
-    if(tofar.checked){
-        temp = Number(textbox.value)
-        temp = temp*9/5+32
-        result.textContent = temp.toFixed(1) + "F"
+function convert() {
+    let temp;
+    if (radio1.checked) {
+        temp = Number(textbox.value);
+        temp = temp * 9/5 + 32;
+        result.textContent = temp.toFixed(1) + " °F";
+    } else if (radio2.checked) {
+        temp = Number(textbox.value);
+        temp = (temp - 32) * 5/9;
+        result.textContent = temp.toFixed(1) + " °C";
+    } else {
+        result.textContent = "PLEASE SELECT A UNIT";
     }
-
-    else if(tocel.checked){
-        temp = Number(textbox.value)
-        temp = temp*9/5+32
-        result.textContent = temp + "C"
-        
-    }
-    else{
-        result.textContent = "PLEASE SELECT A UNIT"
-    }
+    result.classList.remove('show');
+    setTimeout(() => result.classList.add('show'), 250);
 }
