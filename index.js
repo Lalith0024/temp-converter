a5a88187f6a319b4b3f0085e643490e8403067de
Lalith0024 <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-18 11:31:43 +0530

    changed commit

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-const textbox = document.getElementById("textbox")
-const radio1 = document.getElementById("tofar")
-const radio2 = document.getElementById("tocel")
-const result = document.getElementById("result")
+const textbox = document.getElementById("textbox");
+const radio1 = document.getElementById("tofar");
+const radio2 = document.getElementById("tocel");
+const result = document.getElementById("result");
 
-let temp;
-
-function convert(){
-    if(tofar.checked){
-        temp = Number(textbox.value)
-        temp = temp*9/5+32
-        result.textContent = temp.toFixed(1) + "F"
+function convert() {
+    let temp;
+    
+    if (textbox.value === "") {
+        result.textContent = "Please enter some value 🤓";
+        result.classList.remove('show');
+        setTimeout(() => result.classList.add('show'), 250);
+        return; // Exit the function early if no value is entered
     }
-
-    else if(tocel.checked){
-        temp = Number(textbox.value)
-        temp = temp*9/5+32
-        result.textContent = temp + "C"
+    
+    if (radio1.checked) {
+        temp = Number(textbox.value);
+        temp = temp * 9/5 + 32;
+        if (temp > 68){
+            result.textContent = temp.toFixed(1) + " °F" + "🥵";
+            
+        }
+        else{
+            result.textContent = temp.toFixed(1) + " °F" + "🥶";
+        }
+    } else if (radio2.checked) {
+        temp = Number(textbox.value);
+        temp = (temp - 32) * 5/9;
+        if (temp > 68){
+            result.textContent = temp.toFixed(1) + " °C" + "🥵";
+        }
+        else{
+            result.textContent = temp.toFixed(1) + " °C" + "🥶";
+        }
+    } else {
+        result.textContent = "PLEASE SELECT A UNIT 🥹";
+    }
+    result.classList.remove('show');
+    setTimeout(() => result.classList.add('show'), 250);
+    if (isNaN(textbox.value)){
+        result.textContent = "PLEASE ENTER A VALID INTEGER ☝️"
         
     }
-    else{
-        result.textContent = "PLEASE SELECT A UNIT"
-    }
 }
+
